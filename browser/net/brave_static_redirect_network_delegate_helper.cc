07baebba4188f8981749329f62fff9603241e4ef
Pranjal Jumde <pranjal.jumde@gmail.com>
2020-02-03 13:38:30 -0800

    Fix 7976: Adding a proxy for Crowd Deny

--- browser/net/brave_static_redirect_network_delegate_helper.cc
+++ browser/net/brave_static_redirect_network_delegate_helper.cc
@@ -48,6 +48,8 @@ int OnBeforeURLRequest_StaticRedirectWorkForGURL(
       URLPattern::SCHEME_HTTP | URLPattern::SCHEME_HTTPS, kCRXDownloadPrefix);
   static URLPattern autofill_pattern(
       URLPattern::SCHEME_HTTPS, kAutofillPrefix);
+  static URLPattern gvt1_pattern(
+      URLPattern::SCHEME_HTTP | URLPattern::SCHEME_HTTPS, "*://*.gvt1.com/*");
 
 #if BUILDFLAG(ENABLE_BRAVE_TRANSLATE_GO)
   static URLPattern translate_pattern(URLPattern::SCHEME_HTTPS,
@@ -117,6 +119,14 @@ int OnBeforeURLRequest_StaticRedirectWorkForGURL(
     *new_url = request_url.ReplaceComponents(replacements);
     return net::OK;
   }
+
+  if (gvt1_pattern.MatchesURL(request_url)) {
+    replacements.SetSchemeStr("https");
+    replacements.SetHostStr(kBraveRedirectorProxy);
+    *new_url = request_url.ReplaceComponents(replacements);
+    return net::OK;
+  }
+
 #if BUILDFLAG(ENABLE_BRAVE_TRANSLATE_GO)
   if (translate_pattern.MatchesURL(request_url)) {
     replacements.SetQueryStr(request_url.query_piece());
